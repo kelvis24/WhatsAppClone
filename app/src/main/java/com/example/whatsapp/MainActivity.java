3cb67f1e6a8bd0054ce1e7f45881e4edac6352e6
kelvis24 <elviskimara@outlook.com>
2020-06-10 23:57:04 -0500

    Just added a new empty activity_main2.xml

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -59,6 +65,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    userIsLoggedIn();
+            }
+        });
+    }
+
+    private void userIsLoggedIn() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+            finish();
+            return;
+
+        }
     }
 
     private void startPhoneNumberVerification() {
